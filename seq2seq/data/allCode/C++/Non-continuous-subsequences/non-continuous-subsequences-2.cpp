bcd48d1cb28281cbcdaaa1a0ed504d88c2ec0834
Gokul Dhakal <signgokul16@gmail.com>
2018-03-25 22:24:27 -0400

    individual .cpp files added

--- seq2seq/data/allCode/C++/Non-continuous-subsequences/non-continuous-subsequences-2.cpp
+++
@@ -1,4 +0,0 @@
-int main(){
-  N n(4);
-  while (n.hasNext()) std::cout << n.next() << "\t* " << std::bitset<4>(n.next()) << std::endl;
-}
--- seq2seq/data/allCode/C++/Non-continuous-subsequences/non-continuous-subsequences-3.cpp
+++
@@ -1,4 +0,0 @@
-int main(){
-  N n(31);
-  int z{};for (;n.hasNext();++z); std::cout << z << std::endl;
-}
